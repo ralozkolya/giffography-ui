8ccc4a01d48c5ffddc609287392fb0a79ee9ce4e
Kolya <raloz1@gmail.com>
2017-07-12 23:53:59 +0400

    Merge branch 'feature/facebook-share' into develop

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../../services/api.service';
 import { Video } from '../../interfaces/video';
 import { PaginatedResponse } from '../../interfaces/paginated-response';
 import { Event } from '../../interfaces/event';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-event',
@@ -15,23 +16,37 @@ export class EventComponent implements OnInit {
   @Input()
   public event: Event;
 
+  public name: string;
+
   public videos: PaginatedResponse<Video>;
   public loading = false;
   public error: any = null;
 
   constructor(private route: ActivatedRoute,
-              private api: ApiService) { }
+              private api: ApiService,
+              private translate: TranslateService) { }
 
   async ngOnInit() {
+
+    this.translate.onLangChange.subscribe(() => {
+      this.assignName();
+    });
+
     this.loading = true;
     await this.retrieveEvent(this.route.snapshot.params.id);
     await this.retrieveVideos(this.route.snapshot.params.id);
     this.loading = false;
   }
 
+  public isEmpty(): boolean {
+    const v = this.videos;
+    return !(v && v.data && v.data.length);
+  }
+
   private async retrieveEvent(id: number): Promise<void> {
     try {
       this.event = await this.api.getEvent(id);
+      this.assignName();
     } catch (e) {
       this.error = e;
     }
@@ -43,6 +58,11 @@ export class EventComponent implements OnInit {
     } catch (e) {
       this.error = e;
     }
+  }
+
+  private assignName(): void {
+    const lang = this.translate.currentLang;
+    this.name = this.event[`${lang}_name`];
   }
 
 }
